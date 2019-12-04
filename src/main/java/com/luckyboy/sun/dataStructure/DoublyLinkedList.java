125e417dd0a313250eea844ab8bd26989009e828
luckyboy520 <343150987@qq.com>
2019-12-04 23:23:24 +0800

    双向链表add方法

--- src/main/java/com/luckyboy/sun/dataStructure/DoublyLinkedList.java
+++ src/main/java/com/luckyboy/sun/dataStructure/DoublyLinkedList.java
@@ -6,6 +6,15 @@ public class DoublyLinkedList<T> {
     private Node<T> lastNode;
     private int count;
 
+    public static void main(String[] args) {
+        DoublyLinkedList<String> linkedList = new DoublyLinkedList<>();
+        linkedList.add("1");
+        linkedList.add("2");
+        linkedList.add("3");
+        linkedList.add("4");
+        System.out.println(linkedList.toString());
+    }
+
 
     /**
      * @author xieh
@@ -13,9 +22,31 @@ public class DoublyLinkedList<T> {
      * @return
     */
     public int add(T e) {
-        return 0;
+        Node<T> node = null;
+        if(firstNode == null) {
+            node = new Node<>(e, null,null);
+            firstNode = node;
+            lastNode = node;
+        } else {
+            node = new Node<>(e,lastNode,null);
+            lastNode = node;
+        }
+        count++;
+        return count;
     }
 
+    @Override
+    public String toString() {
+        //定义一个辅助节点，不要改变firstNode的值
+        Node<T> node = firstNode;
+        StringBuffer buffer = new StringBuffer();
+        while (node.next != null) {
+            buffer.append(node.node);
+            node = node.next;
+        }
+        buffer.append(node.node);
+        return buffer.toString();
+    }
 
     class Node<T> {
         private T node;
