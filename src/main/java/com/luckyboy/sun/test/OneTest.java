74ad1460663fdabefb8238289d92bf648a6c6f3f
= <343150987@qq.com>
2019-12-11 17:22:15 +0800

    测试用java8流去取两个list的差值

--- src/main/java/com/luckyboy/sun/test/OneTest.java
+++ src/main/java/com/luckyboy/sun/test/OneTest.java
@@ -1,7 +1,67 @@
 package com.luckyboy.sun.test;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+/**
+ * 测试两个list取差值
+ * **/
 public class OneTest {
+    private Integer num;
+
+    public OneTest(Integer num) {
+        this.num = num;
+    }
+
+    public Integer getNum() {
+        return num;
+    }
+
+    public void setNum(Integer num) {
+        this.num = num;
+    }
+
+    @Override
+    public String toString() {
+        return "OneTest{" +
+                "num=" + num +
+                '}';
+    }
+
     public static void main(String[] args) {
-        System.out.println(1);
+        List<OneTest> oneTests1 = new ArrayList<>();
+        List<OneTest> oneTests2 = new ArrayList<>();
+
+        OneTest oneTest1= new OneTest(1);
+        OneTest oneTest2= new OneTest(2);
+        OneTest oneTest3= new OneTest(3);
+        OneTest oneTest4= new OneTest(4);
+        OneTest oneTest5= new OneTest(9);
+        OneTest oneTest12= new OneTest(12);OneTest oneTest13= new OneTest(13);
+        oneTests1.add(oneTest1);oneTests1.add(oneTest2);oneTests1.add(oneTest3);oneTests1.add(oneTest4);oneTests1.add(oneTest5);
+        oneTests1.add(oneTest12);oneTests1.add(oneTest13);
+
+        OneTest oneTest11= new OneTest(1);
+        OneTest oneTest6= new OneTest(2);
+        OneTest oneTest7= new OneTest(3);
+        OneTest oneTest8= new OneTest(4);
+        oneTests2.add(oneTest11);oneTests2.add(oneTest6);oneTests2.add(oneTest7);oneTests2.add(oneTest8);
+
+        List<OneTest> list = oneTests1.stream().filter(oneTest -> {
+            boolean f = false;
+            int count = 0;
+            for(OneTest oneTest9 : oneTests2) {
+                if(oneTest9.getNum().equals(oneTest.getNum())) {
+                    break;
+                }
+                if(count == oneTests2.size() - 1) {
+                    f = true;
+                }
+                count++;
+            }
+            return f;
+        }).collect(Collectors.toList());
+
+        System.out.println(list);
     }
 }
