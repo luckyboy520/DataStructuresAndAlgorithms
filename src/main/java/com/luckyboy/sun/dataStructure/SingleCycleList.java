87abbaef115dc93ac829f134c077ba38a56047b5
= <343150987@qq.com>
2019-12-11 19:01:39 +0800

    单向循环链表思路

---
+++ src/main/java/com/luckyboy/sun/dataStructure/SingleCycleList.java
@@ -0,0 +1,18 @@
+package com.luckyboy.sun.dataStructure;
+
+/**
+ * @author xieh
+ * @date 2019/12/11 17:42
+ */
+public class SingleCycleList {
+    /* 单向循环列表的具体实现暂时不写
+     *   简单思路
+     *      有一个frist节点，一直都是指定第一个元素
+     *      还有一个next节点
+     *      当添加元素时有一个辅助节点指针不断往下走
+     *
+     *      把第几位元素到第几位元素出圈问题，也不是太复杂
+     *         定义两个指针first和first的前一个元素，first前一个和first之间的元素删除
+     *            就是说first前一个的next指定first就可以实现把它们中间的元素删除了
+     *
+     * */}
