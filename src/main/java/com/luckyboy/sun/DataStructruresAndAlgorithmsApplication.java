6ce1b9559df123d984d71804325aea2a709ec73c
xiehui <Hui.Xie3@luxshare-ict.com>
2025-05-23 16:37:30 +0800

    1

--- src/main/java/com/luckyboy/sun/DataStructruresAndAlgorithmsApplication.java
+++ src/main/java/com/luckyboy/sun/DataStructruresAndAlgorithmsApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class DataStructruresAndAlgorithmsApplication {
 
     public static void main(String[] args) {
+        //提交test
         SpringApplication.run(DataStructruresAndAlgorithmsApplication.class, args);
     }
 
